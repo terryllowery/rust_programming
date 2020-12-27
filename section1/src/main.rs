b53eb5b87a00ab0ccacd59d7c8a5301404e98caa
Terry Lowery <tl@lowerytech.net>
2020-12-27 15:58:47 -0600

    Primitive types completed

--- section1/src/main.rs
+++ section1/src/main.rs
@@ -1,3 +1,21 @@
+#![allow(overflowing_literals)]
 fn main() {
-    println!("Hello, world!");
+ let a: u128 = 1;
+ println!("{}", a);
+
+ // OVerflow
+ let a: u8 = 257;
+ println!("257 = {} when overflowing u8 datatype.", a);
+
+ // Floats 
+ let a: f32 = 259.3;
+ println!(" float types: {}", a);
+
+ // bools
+ let a: bool = true;
+ println!("bool true is {}", a);
+
+ // Charcter
+ let a: char = 'a';
+ println!("char a is {}", a);
 }
