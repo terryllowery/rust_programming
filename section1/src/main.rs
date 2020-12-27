c73943ee4d746d7b93bf34abf90dcea05abfdd00
Terry Lowery <tl@lowerytech.net>
2020-12-27 17:36:28 -0600

    primitives

--- section1/src/main.rs
+++ section1/src/main.rs
@@ -1,4 +1,8 @@
 #![allow(overflowing_literals)]
+
+// Constants in global
+const GLOBAL_MAX: i32 = 500;
+
 fn main() {
  let a: u128 = 1;
  println!("{}", a);
@@ -18,4 +22,49 @@ fn main() {
  // Charcter
  let a: char = 'a';
  println!("char a is {}", a);
+
+ // Mutibility
+ // we cannot do this:
+ // a = 'b';
+ // println!("{}", a);
+ // we can do this:
+ let mut b: char = 'a';
+ println!("b is now {}", b);
+ b = 'b';
+ println!("b has been changed to {}", b);
+
+ // constants
+ const MAX: i32 = 10;
+ println!("const value MAX is {}", MAX);
+ println!("Global max is {}", GLOBAL_MAX);
+
+ // Strings
+ let a = "Hello";
+ println!("String {}", a);
+
+ let str: String = String::new();
+ println!("Blank string {}", str);
+ // cannot do this:
+ // str = "blah".to_string();
+ println!("changing string: {}", str);
+
+ // vars are immutiable by default so we need to do this
+ let mut mutiable_string: String = String::new();
+ println!("Mutiable string start: {}", mutiable_string);
+ mutiable_string = "blahblahblah".to_string();
+ println!("Now changed string is: {}", mutiable_string);
+
+ // You can also assign string like this:
+ //let this_string: String = String::from("this string");
+ let this_string=String::from("This string");
+ println!("This string is: {}", this_string);
+
+ // pushing to a string
+ let mut push_string=String::from("blah");
+ println!("String b4 push: {}",push_string );
+ push_string.push_str(" de blah");
+ println!("After the push: {}", push_string);
+
+
+
 }
