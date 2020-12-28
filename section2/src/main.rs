79b90c2d8f8de863625a385ed1066da2e2729b73
Terry Lowery <tl@lowerytech.net>
2020-12-27 19:50:49 -0600

    if section completed

--- section2/src/main.rs
+++ section2/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 10;
+
+    if a % 2 == 0 {
+        println!("Even");
+    } else {
+        println!("Odd");
+    }
+
+    // else if flows
+    if a == 0 {
+        println!("A is 0");
+    } else if a < 5 {
+        println!("a is less then 5");
+   } else if a % 2 == 0 {
+       println!("a is even");
+   } else {
+       println!("a is odd");
+   }
+
+   let var_name = if a % 2 == 0 {
+       "Even" // no semicolon
+   } else {
+       "Odd" // no semicolon
+   }; // must be closed by semicolon since it is a statement
+
+   println!("in let var_name a is: {}", var_name);
 }
